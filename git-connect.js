cb634c89f8222525016bc475ee3aaba9b9484eb2
krispo <konstantin.skipor@gmail.com>
2014-06-13 23:58:02 +0600

    added toggle

--- git-connect.js
+++ git-connect.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 /*
- * Javascript plugin for token-base Github OAuth via proxy with cookie storage.
+ * Javascript plugin for providing token-base Github OAuth via proxy with cookie storage.
  * https://github.com/krispo/git-connect
  */
 (function(){
@@ -26,13 +26,13 @@
         var code = connection.parseUrl('code');
         if (code){
             get_request(connection.config['proxy'] + '/github_access_token?code=' + code + '&client_id=' + connection.config['client_id'], function(error, data){
+                var access_token = data['access_token'];
                 if (error === null) {
-                    var access_token = data['access_token'];
                     get_request('https://api.github.com/user?access_token=' + access_token, function(error, data){
                         if (error === null){
-                            var login = data['login'];
-                            connection.setCookie("login", login, 7 );
-                            connection.setCookie("access_token", access_token, 7 );
+                            connection.setCookie('login', data['login'], 7 );
+                            connection.setCookie('access_token', access_token, 7 );
+                            window.location.href = window.location.href.replace(/[\\?&]code=[^&#]*/, '');
                         } else console.log(error);
                     })
                 } else console.log(error);
@@ -44,31 +44,37 @@
 
     // Check if user is connected to github
     connection.isConnected = function(){
-        return  connection.getCookie("access_token") !== "";
+        return  connection.getCookie('access_token') !== '';
     };
 
     // Connect to github
     connection.connect = function(){
-        location.href = "https://github.com/login/oauth/authorize?client_id=" + connection.config['client_id'];
+        window.location.href = 'https://github.com/login/oauth/authorize?client_id=' + connection.config['client_id'];
     };
 
     // Disconnect from github
     connection.disconnect = function(){
-        connection.deleteCookies(["login", "access_token"]);
+        connection.deleteCookies(['login', 'access_token']);
+        window.location.reload();
+    };
+
+    // Connection toggle
+    connection.toggle = function(){
+        connection.isConnected() ? connection.disconnect() : connection.connect();
     };
 
     // Get credentials: login, access_token
     connection.getCredentials = function(){
         return {
-            login: connection.getCookie("login"),
-            access_token: connection.getCookie("access_token")
+            login: connection.getCookie('login'),
+            access_token: connection.getCookie('access_token')
         }
     };
 
     // Get 'param' from url
     connection.parseUrl = function(param){
-        param = param.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-        var regex = new RegExp( "[\\?&]"+param+"=([^&#\/]*)" );
+        param = param.replace(/[\[]/,'\\\[').replace(/[\]]/,'\\\]');
+        var regex = new RegExp( '[\\?&]'+param+'=([^&#\/]*)' );
         var results = regex.exec( window.location.href );
         return results === null ? null : results[1];
     };
@@ -77,19 +83,19 @@
     connection.setCookie = function(name, value, expires) {
         var d = new Date();
         d.setTime(d.getTime() + (expires*24*60*60*1000));
-        var expires = "expires=" + d.toGMTString();
-        document.cookie = name + "=" + value + "; " + expires;
+        var expires = 'expires=' + d.toGMTString();
+        document.cookie = name + '=' + value + '; ' + expires;
     };
 
     // Get cookie by name
     connection.getCookie = function(name) {
-        var name = name + "=";
+        var name = name + '=';
         var ca = document.cookie.split(';');
         for(var i=0; i<ca.length; i++) {
             var c = ca[i].trim();
             if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
         }
-        return "";
+        return '';
     };
 
     // Delete all cookies from array - names
